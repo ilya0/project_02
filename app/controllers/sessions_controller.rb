36c7f9d6b685ff85f4cf2cba5751c1877c25ad24
ilya osovets <iosovets@gmail.com>
2016-04-13 18:17:37 -0700

    routes and show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,11 @@ class SessionsController < ApplicationController
 
 
   def create
-
     #creates a user variable and finds the user by the email paramerter
-    user = User.find_by({
-      email: params[:email]
-      })
+    user = User.find_by({email: params[:email]})
 # if user is false or user authenicate params password is false redirect to the root
     if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-
+      session[:user_id] = user.id
       redirect_to "/userlistings"
     else
       render :new
