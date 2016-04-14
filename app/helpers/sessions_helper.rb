4ec9a259405c2d33cce640e3199ec4dcfeb51fae
ilya osovets <iosovets@gmail.com>
2016-04-13 20:30:52 -0700

    working lockout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   def current_user
     #if the @currentuser database is nil then find the user id by the user_id
-    @currentuser ||= User.find_by({
+    @current_user ||= User.find_by({
       id:session[:user_id]
       })
   end
