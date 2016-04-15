83fff35d99df1b6131c4dcef917e9bef9a05c1d6
ilya osovets <iosovets@gmail.com>
2016-04-14 19:13:51 -0700

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,27 @@
 class UsersController < ApplicationController
-  before_action :if_login, only: [:show]
+  # before_action :if_login, only: [:show]
   def index
     @users = User.all
   end
 
   def new
     @user = User.new
-
   end
 
   def create
-    @user = User.new({
-      email: params[:email],
-      password: params[:password],
-      password_confirmation:params[:password_confirmation]
-      })
+    # @user = User.new({
+    #   email: params[:email],
+    #   password: params[:password],
+    #   password_confirmation:params[:password_confirmation]
+    #   })
+
+    @user = User.new(user_params)
 
     if @user.save
       # create a cookie when you create a user
       session[:user_id] = @user.id
       #redirect to the root
-      redirect_to "/"
+      redirect_to '/'
     else
       #if everythng is wrong re-render page by instanciating the new method
       render :new
@@ -54,7 +55,7 @@ def edit
 
   private
   def user_params
-    params.require(:user).permit(:name,:location,:sex,:phone_number,:email,:website,:fblink,:sclink,:twlink,)
+    params.require(:user).permit(:name,:password,:password_confirmation,:location,:sex,:phone_number,:email,:website,:fblink,:sclink,:twlink)
   end
 
   def if_login
